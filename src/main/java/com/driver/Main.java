1714d78c05371e89b434f91af16ecccecfc95f97
viraj <virajkate46@gmail.com>
2022-12-11 20:50:35 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+//    obj.name="hello";
+//    System.out.println(obj.name);
+//    name has private access in com.driver.RWOnly
+    obj.setName("hello");
+    System.out.println(obj.getName());
+  }
 }
